de9265d53e9b7db45b57c170ccacf7297091ebdf
April Something <thinhphanvn8499@gmail.com>
2023-12-19 14:13:14 +0700

    Update redux-thunk

--- redux/redux-thunk/src/App.js
+++ redux/redux-thunk/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import User from "./components/User";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/users" element={<User />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
