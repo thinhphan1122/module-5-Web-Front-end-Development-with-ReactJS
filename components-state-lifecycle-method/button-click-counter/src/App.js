7d732441713093a0ac9e409ff7c940ec99ee8a59
April Something <thinhphanvn8499@gmail.com>
2023-12-20 06:14:29 +0700

    Install yarn in module 5 and add button-click-counter

---
+++ components-state-lifecycle-method/button-click-counter/src/App.js
@@ -0,0 +1,52 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+    };
+  }
+
+  increase = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+  decrease = () => {
+    this.setState({ number: this.state.number - 1 });
+  };
+
+  render() {
+    return (
+      <div style={{ textAlign: "center", padding: 30 }}>
+        <button onClick={this.decrease}>Decrease</button>
+        <span style={{ padding: 10 }}>{this.state.number}</span>
+        <button onClick={this.increase}>Increase</button>
+      </div>
+    );
+  }
+}
+
+export default App;
